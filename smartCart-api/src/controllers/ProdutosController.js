46c601e92d173bf45b55e12bbbee0e0369ede458
Your Name <larissalanes@Larissas-MacBook-Air.local>
2023-11-13 19:07:19 -0300

    configuracao do projeto

---
+++ smartCart-api/src/controllers/ProdutosController.js
@@ -0,0 +1,1 @@
+const ProdutosServices = require('../services/ProdutosServices');
---
+++ smartCart-api/src/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const ProdutosController = require('./controllers/ProdutosController');
+
+module.exports = router;
